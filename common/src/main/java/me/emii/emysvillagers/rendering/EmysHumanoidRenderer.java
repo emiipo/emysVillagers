d3eb418f8af18fde9325e87f92a842b87b432eaf
emi <emii.povi@gmail.com>
2026-02-27 04:23:23 +0000

    added most basic name tag rendering

--- common/src/main/java/me/emii/emysvillagers/rendering/EmysHumanoidRenderer.java
+++ common/src/main/java/me/emii/emysvillagers/rendering/EmysHumanoidRenderer.java
@@ -1,8 +1,12 @@
 package me.emii.emysvillagers.rendering;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Mob;
 
@@ -16,6 +20,22 @@ public class EmysHumanoidRenderer<T extends Mob> extends MobRenderer<T, EmysHuma
 
     public EmysHumanoidRenderer(EntityRendererProvider.Context context, float shadowRadius) {
         super(context, new EmysHumanoidModel<>(context.bakeLayer(HUMANOID)), shadowRadius);
+    }
+
+    @Override
+    protected void renderNameTag(T entity, Component displayName, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, float partialTick) {
+        Component customName = Component.literal("qtepie");
+        super.renderNameTag(entity, customName, poseStack, bufferSource, packedLight, partialTick);
+    }
+
+    @Override                                                                                            
+    protected boolean shouldShowName(T entity) {
+        return true;                                                                                     
+    }   
+
+    @Override
+    protected void scale(T entity, PoseStack poseStack, float partialTick) {
+        poseStack.scale(0.9375f, 0.9375f, 0.9375f);
     }
 
     @Override
