a093a0ba241c9258617390761953faef9fd2326c
emi <emii.povi@gmail.com>
2026-02-27 00:28:09 +0000

    info changeup

---
+++ neoforge/src/main/java/me/emii/emysvillagers/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package me.emii.emysvillagers.platform;
+
+import me.emii.emysvillagers.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
