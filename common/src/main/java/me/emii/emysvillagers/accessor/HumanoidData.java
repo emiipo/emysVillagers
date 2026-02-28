0ddc28bffb505e4e7a13b5dc792862d7790ff632
emi <emii.povi@gmail.com>
2026-02-28 07:03:28 +0000

    humanoid data accessor

---
+++ common/src/main/java/me/emii/emysvillagers/accessor/HumanoidData.java
@@ -0,0 +1,5 @@
+package me.emii.emysvillagers.accessor;
+
+import me.emii.emysvillagers.HumanoidManager.BodyType;
+
+public record HumanoidData(BodyType bodyType, String name) {}
---
+++ common/src/main/java/me/emii/emysvillagers/accessor/IHumanoidDataAccessor.java
@@ -0,0 +1,7 @@
+package me.emii.emysvillagers.accessor;
+
+public interface IHumanoidDataAccessor {
+    HumanoidData emysvillagers$getData();
+    void emysvillagers$setData(HumanoidData data);
+    void emysvillagers$setName(String name);
+}
