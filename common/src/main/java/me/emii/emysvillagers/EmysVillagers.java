c5bf5ed45c098d94a452bb3e62b8785505604ac2
emi <emii.povi@gmail.com>
2026-02-28 04:11:11 +0000

    added tag helper loading

--- common/src/main/java/me/emii/emysvillagers/EmysVillagers.java
+++ common/src/main/java/me/emii/emysvillagers/EmysVillagers.java
@@ -1,14 +1,25 @@
 package me.emii.emysvillagers;
 
+import java.util.ServiceLoader;
+import me.emii.emysvillagers.tags.ITagHelper;
+
 public class EmysVillagers {
 
     public static final String MOD_ID = "emysvillagers";
     public static final String MOD_NAME = "emys Villagers";
 
     public static HumanoidManager HUMANOID_MANAGER = new HumanoidManager();
+    public static final ITagHelper TAG_HELPER = load(ITagHelper.class);
 
     public static void init() {
         Log.info("Loaded :)");
+    }
+
+    private static <T> T load(Class<T> classToLoad) {
+        final T loadedService = ServiceLoader.load(classToLoad)
+                .findFirst()
+                .orElseThrow(() -> new NullPointerException("Failed to load service for " + classToLoad.getName()));
+        return loadedService;
     }
 
 }
