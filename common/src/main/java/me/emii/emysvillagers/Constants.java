471a77b16af7c46f7daeddfca1be75401b77e0bf
emi <emii.povi@gmail.com>
2026-03-01 01:21:58 +0000

    updated constants

--- common/src/main/java/me/emii/emysvillagers/Constants.java
+++ common/src/main/java/me/emii/emysvillagers/Constants.java
@@ -3,4 +3,5 @@ package me.emii.emysvillagers;
 public class Constants {
     public static final String BODY_TYPE = "BodyType";
     public static final String NAME = "Name";
+    public static final String PITCH = "Pitch";
 }
